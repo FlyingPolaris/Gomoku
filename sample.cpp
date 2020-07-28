50a46b30ea63350007f84db861ab402fca3b0f80
FlyingPolaris <62804441+FlyingPolaris@users.noreply.github.com>
2020-07-28 15:38:36 +0800

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <climits>
 
-
+typedef long long ll;
 extern int ai_side; //0: black, 1: white
 std::string ai_name = "polaris'_ai";
 
@@ -30,16 +30,27 @@ int positionweight[15][15] =
 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 };
 
+ll judge_score(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return pow(10, n);
+    if (n == 3) return pow(100, n);
+    if (n == 4) return pow(1000, n);
+    return 1e16;
+}
+
+
 int dir[4][2] =
 {
 { 0, 1 },
 { 1, 0 },
 { 1, 1 },
-{ -1, 1 },
+{ 1, -1 },
 };
-const int DEPTH = 4;
-int x_place, y_place;
-int score[2] = { 0,0 };
+
+const int DEPTH = 6;
+int x_place = 0, y_place = 0;
+ll score[2] = { 0,0 };
 int range[4] = { 14,0,14,0 };
 
 void range_update(int* range, int x, int y)
@@ -47,18 +58,22 @@ void range_update(int* range, int x, int y)
     if (x - 2 >= 0)
     {
         if (x - 2 < range[0]) range[0] = x - 2;
+        else range[0] = 0;
     }
     if (x + 2 < 15)
     {
         if (x + 2 > range[1]) range[1] = x + 2;
+        else range[1] = 14;
     }
     if (y - 2 >= 0)
     {
         if (y - 2 < range[2]) range[2] = y - 2;
+        else range[2] = 0;
     }
     if (y + 2 < 15)
     {
         if (y + 2 > range[3]) range[3] = y + 2;
+        else range[3] = 14;
     }
 }
 
@@ -71,14 +86,14 @@ bool outofboard(int x, int y)
 }
 
 
-bool search_place(int a[15][15], int x, int y)
+bool search_place(int x, int y)
 {
-    if (a[x][y] != -1) return false;
+    if (board[x][y] != -1) return false;
     if (outofboard(x, y)) return false;
     for (int i = x - 2;i <= x + 2;++i)
         for (int j = y - 2;j <= j + 2;++j)
         {
-            if (!outofboard(i, j) && (a[i][j] == 0 || a[i][j] == 1))
+            if (!outofboard(i, j) && (board[i][j] == 0 || board[i][j] == 1))
                 return true;
         }
     return false;
@@ -261,60 +276,60 @@ bool search_place(int a[15][15], int x, int y)
 //}
 
 
-int score_update(int x, int y, int color, int* score)
+int score_update(int x, int y, int color, ll* score)
 {
     bool left_blocked = false, right_blocked = false;
     int num1 = 0, num2 = 0, num = 0;
-    int weight1 = 0, weight2 = 0, weight = 0;
+    //int weight1 = 0, weight2 = 0, weight = 0;
     for (int i = 0;i < 4;++i)
     {
         int det_x = dir[i][0], det_y = dir[i][1];
         while (!outofboard(x - det_x * (1 + num1), y - det_y * (1 + num1)))
         {
             if (board[x - det_x * (1 + num1)][y - det_y * (1 + num1)] != color) break;
-            weight1 += positionweight[x - det_x * (1 + num1)][y - det_y * (1 + num1)];
+            //weight1 += positionweight[x - det_x * (1 + num1)][y - det_y * (1 + num1)];
             num1++;
         }
-        weight += weight1;
-        if(num1) weight1 /= num1;
+       // weight += weight1;
+       // if(num1) weight1 /= num1;
         while (!outofboard(x + det_x * (1 + num2), y + det_y * (1 + num2)))
         {
             if (board[x + det_x * (1 + num2)][y + det_y * (1 + num2)] != color) break;
-            weight2 += positionweight[x + det_x * (1 + num2)][y + det_y * (1 + num2)];
+           // weight2 += positionweight[x + det_x * (1 + num2)][y + det_y * (1 + num2)];
             num2++;
         }
-        weight += weight2;
-        if (num2) weight2 /= num2;
+      //  weight += weight2;
+       // if (num2) weight2 /= num2;
 
-        weight += positionweight[x][y];
+      //  weight += positionweight[x][y];
         num = num1 + num2 + 1;
-        weight /= num;
+      //  weight /= num;
 
         if (outofboard(x - det_x * (1 + num1), y - det_y * (1 + num1)) || board[x - det_x * (1 + num1)][y - det_y * (1 + num1)] != -1) left_blocked = true;
         if (outofboard(x + det_x * (1 + num2), y + det_y * (1 + num2)) || board[x + det_x * (1 + num2)][y + det_y * (1 + num2)] != -1) right_blocked = true;
 
-        score[color] -= pow(10, num1 - left_blocked) * weight1;
-        score[color] -= pow(10, num2 - right_blocked) * weight2;
+        score[color] -= judge_score(num1 - left_blocked);//* weight1;
+        score[color] -= judge_score(num2 - right_blocked);// *weight2;
 
-        if (num == 5) score[color] += 1e7;
-        else if (left_blocked + right_blocked < 2) score[color] += pow(10, num - left_blocked - right_blocked) * weight;
+        if (num == 5) score[color] += judge_score(5);
+        else if (left_blocked + right_blocked < 2) score[color] += judge_score(num + 1 - left_blocked - right_blocked);// *weight;
 
-        weight1 = weight2 = 0;
+        //weight1 = weight2 = 0;
         if (!num1 && left_blocked)
         {
             while (!outofboard(x - det_x * (1 + num1), y - det_y * (1 + num1)))
             {
-                if (board[x - det_x * (1 + num1)][y - det_y * (1 + num1)] != ~color) break;
-                weight1 += positionweight[x - det_x * (1 + num1)][y - det_y * (1 + num1)];
+                if (board[x - det_x * (1 + num1)][y - det_y * (1 + num1)] != 1 - color) break;
+               // weight1 += positionweight[x - det_x * (1 + num1)][y - det_y * (1 + num1)];
                 num1++;
             }
-            if(num1) weight1 /= num1;
+           // if(num1) weight1 /= num1;
 
             if (outofboard(x - det_x * (1 + num1), y - det_y * (1 + num1)) || board[x - det_x * (1 + num1)][y - det_y * (1 + num1)] != -1) left_blocked = true;
             else left_blocked = false;
 
-            score[~color] -= pow(10, num1 - left_blocked) * weight1;
-            if (!left_blocked) score[~color] += pow(10, num1 - 1) * weight1;
+            score[1 - color] -= judge_score(num1 - left_blocked);// *weight1;
+            if (!left_blocked) score[1 - color] += judge_score(num1 - 1);// *weight1;
         }
 
         if (!num2 && right_blocked)
@@ -322,19 +337,20 @@ int score_update(int x, int y, int color, int* score)
             while (!outofboard(x + det_x * (1 + num2), y + det_y * (1 + num2)))
             {
                 if (board[x + det_x * (1 + num2)][y + det_y * (1 + num2)] != color) break;
-                weight2 += positionweight[x + det_x * (1 + num2)][y + det_y * (1 + num2)];
+             //   weight2 += positionweight[x + det_x * (1 + num2)][y + det_y * (1 + num2)];
                 num2++;
             }
-            if(num2) weight2 /= num2;
+           // if(num2) weight2 /= num2;
 
             if (outofboard(x + det_x * (1 + num2), y + det_y * (1 + num2)) || board[x + det_x * (1 + num2)][y + det_y * (1 + num2)] != -1) right_blocked = true;
             else right_blocked = false;
 
-            score[~color] -= pow(10, num2 - right_blocked) * weight2;
-            if (!right_blocked) score[~color] += pow(10, num2 - 1) * weight2;
+            score[1 - color] -= judge_score(num2 - right_blocked);// *weight2;
+            if (!right_blocked) score[1 - color] += judge_score(num2 - 1);// *weight2;
         }
     }
-    return score[color] - score[~color];
+
+    return score[ai_side] - score[1 - ai_side];
 }
 
 struct tree
@@ -343,94 +359,89 @@ struct tree
     int w;
 };
 
-int min_max(int deep, int color, int ALPHA, int BETA, int _range[4], int _score[2])
+ll min_max(int deep, int color, int ALPHA, int BETA, int _range[4], ll _score[2])
 {
     tree t;
-    t.w = INT_MIN;
+    //t.w = INT_MIN;
     t.alpha = ALPHA;
     t.beta = BETA;
     int minmax;
     bool flag = false;
     int range[4];
-    int score[2];
+    ll score[2];
 
 
-    if (deep % 2 == 0)
+
+    if (deep % 2 == 0 && deep != 0)
     {
         for (int i = _range[0];i <= _range[1];++i)
         {
-            int breakflag = false;
+            bool breakflag = false;
             for (int j = _range[2];j <= _range[3];++j)
             {
-                if (search_place(board, i, j))
+                if (board[i][j] == -1)
                 {
                     memcpy(range, _range, 4 * sizeof(int));
-                    memcpy(score, _score, 2 * sizeof(int));
+                    memcpy(score, _score, 2 * sizeof(ll));
                     board[i][j] = color;
                     range_update(range, i, j);
 
                     score_update(i, j, color, score);
-                    minmax = min_max(deep - 1, ~color, t.alpha, t.beta, range, score);
 
+                    minmax = min_max(deep - 1, 1 - color, t.alpha, t.beta, range, score);
                     board[i][j] = -1;
 
                     if (t.alpha < minmax)
                     {
                         t.alpha = minmax;
-                        if (deep == DEPTH)
-                        {
-                            x_place = i;
-                            y_place = j;
-                        }
+                        t.x = i;
+                        t.y = j;
                     }
-                    if (t.beta <= t.alpha)
-                    {
-                        breakflag = true;
-                        break;
-                    }
+
+                    if (t.beta <= t.alpha) breakflag = true;
                 }
+                if (breakflag) break;
             }
-            if (breakflag) break;
         }
-        return -t.alpha;
     }
 
-    if (deep % 2 != 0)
+    if (deep % 2 == 1)
     {
         for (int i = _range[0];i <= _range[1];++i)
         {
-            int breakflag = false;
+            bool breakflag = false;
             for (int j = _range[2];j <= _range[3];++j)
             {
-                if (search_place(board, i, j))
+                if (board[i][j] == -1)
                 {
                     memcpy(range, _range, 4 * sizeof(int));
-                    memcpy(score, _score, 2 * sizeof(int));
+                    memcpy(score, _score, 2 * sizeof(ll));
                     board[i][j] = color;
                     range_update(range, i, j);
 
-                    if (deep == 1) minmax = score_update(i, j, color, score);
-                    else
-                    {
-                        score_update(i, j, color, score);
-                        minmax = min_max(deep - 1, ~color, t.alpha, t.beta, range, score);
-                    }
+                    score_update(i, j, color, score);
 
+                    minmax = min_max(deep - 1, 1 - color, t.alpha, t.beta, range, score);
                     board[i][j] = -1;
 
-                    if (t.beta > minmax) t.beta = minmax;
-                    if (t.beta <= t.alpha)
+                    if (t.beta > minmax)
                     {
-                        breakflag = true;
-                        break;
+                        t.beta = minmax;
+                        t.x = i;
+                        t.y = j;
                     }
+
+                    if (t.beta <= t.alpha) breakflag = true;
                 }
+                if (breakflag) break;
             }
-            if (breakflag) break;
         }
-        return -t.beta;
     }
-    return -minmax;
+
+    x_place = t.x;
+    y_place = t.y;
+    return score[ai_side] - score[1 - ai_side];
+
 }
 
 //init function is called once at the beginning
@@ -461,7 +472,7 @@ void init()
 
 bool swap_or_not()
 {
-    if (score[~ai_side] <= 80) return false;
+    if (score[1 - ai_side] <= 80) return false;
     else return true;
 }
 
@@ -477,14 +488,14 @@ std::pair<int, int> action(std::pair<int, int> loc)
             board[7][7] = ai_side;
             range[0] = range[2] = 5;
             range[1] = range[3] = 9;
-            score[~ai_side] += 50;
+            score[1 - ai_side] += 50;
             return std::make_pair(7, 7);
         }
         else if (turn == 3)
         {
             for (int i = 0; i < 15; ++i)
                 for (int j = 0; j < 15; ++j)
-                    if (board[i][j] != -1)  board[i][j] = ~board[i][j];
+                    if (board[i][j] != -1)  board[i][j] = 1 - board[i][j];
             int tmp = score[0];
             score[0] = score[1];
             score[1] = tmp;
@@ -497,8 +508,8 @@ std::pair<int, int> action(std::pair<int, int> loc)
         }
     }
 
-    board[loc.first][loc.second] = ~ai_side;
-    score_update(loc.first, loc.second, ~ai_side, score);
+    board[loc.first][loc.second] = 1 - ai_side;
+    score_update(loc.first, loc.second, 1 - ai_side, score);
     range_update(range, loc.first, loc.second);
 
     if (turn == 1)
@@ -513,9 +524,9 @@ std::pair<int, int> action(std::pair<int, int> loc)
             for (int i = 6;i <= 8;++i)
                 for (int j = 6;j <= 8;++j)
                 {
-                    if (board[i][j] == ~ai_side) flag = true;
+                    if (board[i][j] == 1 - ai_side) flag = true;
                 }
-            if (flag) score[~ai_side] -= 40;
+            if (flag) score[1 - ai_side] -= 40;
             else score[ai_side] += 50;
 
             return std::make_pair(7, 7);
@@ -529,7 +540,7 @@ std::pair<int, int> action(std::pair<int, int> loc)
                 range[1] = 9;
                 range[2] = 4;
                 range[3] = 8;
-                score[~ai_side] -= 50;
+                score[1 - ai_side] -= 50;
                 return std::make_pair(7, 6);
             }
             else
@@ -537,7 +548,7 @@ std::pair<int, int> action(std::pair<int, int> loc)
                 board[6][6] = ai_side;
                 range[0] = range[2] = 4;
                 range[1] = range[3] = 8;
-                score[~ai_side] -= 50;
+                score[1 - ai_side] -= 50;
                 return std::make_pair(6, 6);
             }
         }
@@ -547,7 +558,7 @@ std::pair<int, int> action(std::pair<int, int> loc)
     {
         for (int i = 0; i < 15; ++i)
             for (int j = 0; j < 15; ++j)
-                if (board[i][j] != -1)  board[i][j] = ~board[i][j];
+                if (board[i][j] != -1)  board[i][j] = 1 - board[i][j];
 
         int tmp = score[0];
         score[0] = score[1];
@@ -562,6 +573,3 @@ std::pair<int, int> action(std::pair<int, int> loc)
     range_update(range, x_place, y_place);
     return std::make_pair(x_place, y_place);
 }
-
-
-
